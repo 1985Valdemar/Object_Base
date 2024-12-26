b0cd021d78d5492d855ae69e8ad76ee7cb10294c
1985Valdemar <vteider@yahoo.com>
2024-12-26 09:35:22 -0300

    Atualizando Projeto Com Forms

---
+++ Object.ConsoleApp1/teste/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace teste
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(369, 348);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(241, 101);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Filho";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(122, 348);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(241, 101);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Pai";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::teste.Properties.Resources.Familia;
+            this.pictureBox1.Location = new System.Drawing.Point(135, 66);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(241, 183);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(896, 494);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Location = new System.Drawing.Point(300, 150);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Object.ConsoleApp1/teste/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace teste
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Defina o caminho base onde suas imagens estão localizadas
+            string basePath = "C:\\Users\\Acer\\Documents\\0.0 - C#_Curso\\object_Base\\Object.ConsoleApp1\\teste\\Resources\\";
+
+            // Nome da imagem (pode ser obtido de uma entrada do usuário ou de uma variável)
+            string imageName = "Daniii.jpg";
+
+            // Combinar o caminho base com o nome da imagem
+            string imagePath = System.IO.Path.Combine(basePath, imageName);
+
+            // Verifica se o arquivo existe antes de tentar carregar
+            if (System.IO.File.Exists(imagePath))
+            {
+                // Carrega a imagem e define no PictureBox
+                pictureBox1.Image = Image.FromFile(imagePath);
+                // Ajusta o tamanho da imagem ao tamanho do PictureBox
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            else
+            {
+                // Mensagem de erro se a imagem não for encontrada
+                MessageBox.Show("Imagem não encontrada: " + imagePath);
+            }
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            // Defina o caminho base onde suas imagens estão localizadas
+            string basePath = "C:\\Users\\Acer\\Documents\\0.0 - C#_Curso\\object_Base\\Object.ConsoleApp1\\teste\\Resources\\";
+
+            // Nome da imagem (pode ser obtido de uma entrada do usuário ou de uma variável)
+            string imageName = "Familia.jpg";
+
+            // Combinar o caminho base com o nome da imagem
+            string imagePath = System.IO.Path.Combine(basePath, imageName);
+
+            // Verifica se o arquivo existe antes de tentar carregar
+            if (System.IO.File.Exists(imagePath))
+            {
+                // Carrega a imagem e define no PictureBox
+                pictureBox1.Image = Image.FromFile(imagePath);
+                // Ajusta o tamanho da imagem ao tamanho do PictureBox
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            else
+            {
+                // Mensagem de erro se a imagem não for encontrada
+                MessageBox.Show("Imagem não encontrada: " + imagePath);
+            }
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
